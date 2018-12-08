f06e36de0929f3cdcd29dd6b65a9d064c34a8c7f
Sep Dehpour <sep@zepworks.com>
2018-12-07 17:06:30 -0800

    demo can take multiple modules now

--- fast_autocomplete/demo.py
+++ fast_autocomplete/demo.py
@@ -2,12 +2,14 @@ from pprint import pprint
 from fast_autocomplete.misc import read_single_keypress
 
 
-def demo(autocomplete, max_cost, size):
+def demo(running_modules, max_cost, size):
     """
     Gets an Autocomplete instance that has already data in it and you can then run search on it in real time
     """
 
     word_list = []
+
+    running_modules = running_modules if isinstance(running_modules, dict) else {running_modules.__class__.__name__: running_modules}
 
     print('FAST AUTOCOMPLETE DEMO')
     print('Press any key to search for. Press ctrl+c to exit')
@@ -25,6 +27,8 @@ def demo(autocomplete, max_cost, size):
         joined = ''.join(word_list)
         print(chr(27) + "[2J")
         print(joined)
-        results = autocomplete.search(word=joined, max_cost=max_cost, size=size)
+        results = {}
+        for module_name, module in running_modules.items():
+            results[module_name] = module.search(word=joined, max_cost=max_cost, size=size)
         pprint(results)
         print('')
