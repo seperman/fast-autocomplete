0897bdbab672d929f377d128c81683d5aa612827
Sep Dehpour <sep@zepworks.com>
2019-02-25 13:57:35 -0800

    remove invalid chars

--- fast_autocomplete/__init__.py
+++ fast_autocomplete/__init__.py
@@ -1,5 +1,5 @@
 # flake8: noqa
-__version__ = '0.2.7'
+__version__ = '0.2.8'
 import sys
 pyversion = float(sys.version[:3])
 if pyversion < 3.6:
@@ -8,4 +8,4 @@ if pyversion < 3.6:
 from fast_autocomplete.dwg import AutoComplete
 from fast_autocomplete.draw import DrawGraphMixin
 from fast_autocomplete.demo import demo
-from fast_autocomplete.normalize import normalize_node_name
+from fast_autocomplete.normalize import normalize_node_name, remove_any_special_character
